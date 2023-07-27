6f853ad7f193dac662c56c617ef0627d7844b85b
Kissmart Tshabalala <125476899+Kissmart@users.noreply.github.com>
2023-07-27 02:52:01 +0200

    Add files via upload

---
+++ Arithmetic.c
@@ -0,0 +1,23 @@
+int number,divisor;
+bool is_a_divisor;
+/* ... */
+if (/* condition */)
+{
+    /* code */
+}
+
+(
+/* ... */
+)
+
+{
+cout << "Indeed, " << divisor
+<< " is a divisor of "
+<< number << ’\n’;
+}
+else
+{
+cout << "No, " << divisor
+<< " is not a divisor of "
+<< number << ’\n’;
+}
---
+++ hello world.py
@@ -0,0 +1,3 @@
+print('Hello')
+name = input('kissmart')
+print('Hello.'.kissmart)
